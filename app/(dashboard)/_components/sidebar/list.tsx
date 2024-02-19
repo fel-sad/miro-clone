1efc19a1a628575712687eca14800c28c6e39740
Felix Sadowski <sadowski.felix@gmail.com>
2024-02-19 19:54:49 +0100

    added list for organisations

---
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useOrganizationList } from '@clerk/nextjs';
+
+export const List = () => {
+  const { userMemberships } = useOrganizationList({
+    userMemberships: { infinite: true },
+  });
+
+  if (!userMemberships.data?.length) return null;
+
+  return (
+    <ul className="space-y-4">
+      {userMemberships.data?.map((mem) => (
+        <li key={mem.organization.id}> {mem.organization.name} </li>
+      ))}
+    </ul>
+  );
+};

43113e2447eca37eaad5e0c095016a38b61152d6
Felix Sadowski <sadowski.felix@gmail.com>
2024-02-20 14:02:52 +0100

    added search input and router connection

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { UserButton } from '@clerk/nextjs';
+import { SearchInput } from './sidebar/search-input';
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex lg:flex-1">{/* Add Search */}</div>
+    <div className="flex items-center gap-x-4 p-5 ">
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
       <div className="block lg">
         <UserButton />
       </div>

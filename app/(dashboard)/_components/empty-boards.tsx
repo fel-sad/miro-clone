85dccc80458d951c6cd15719939081f9216e94ef
Felix Sadowski <sadowski.felix@gmail.com>
2024-02-20 21:30:34 +0100

    wired up create board with convex db

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,13 +1,28 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import { api } from '@/convex/_generated/api';
+import { useMutation } from 'convex/react';
+import { useOrganization } from '@clerk/nextjs';
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const create = useMutation(api.board.create);
+  const onClick = () => {
+    if (!organization) return;
+
+    create({ orgId: organization.id, title: 'Untitled' });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" height={110} width={110} alt="Empty" />
       <h2 className="text-2xl font-semibold mt-6">Create your first board </h2>
       <div className="mt-6">
-        <Button size="lg">Create a board</Button>
+        <Button onClick={onClick} size="lg">
+          Create a board
+        </Button>
       </div>
     </div>
   );

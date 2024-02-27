9cb0dec2799c0837853d3564650c2b176c9388c9
Felix Sadowski <sadowski.felix@gmail.com>
2024-02-27 18:17:25 +0100

    layer setup done

--- components/room.tsx
+++ components/room.tsx
@@ -5,6 +5,8 @@ import { RoomProvider } from '@/liveblocks.config';
 import { ClientSideSuspense } from '@liveblocks/react';
 import { Client } from '@clerk/nextjs/server';
 import { Loading } from './ui/auth/loading';
+import { LiveMap, LiveObject, LiveList } from '@liveblocks/client';
+import { Layer } from '@/types/canvas';
 
 interface RoomProps {
   children: ReactNode;
@@ -14,7 +16,14 @@ interface RoomProps {
 
 export const Room = ({ children, roomId, fallback }: RoomProps) => {
   return (
-    <RoomProvider id={roomId} initialPresence={{ cursor: null }}>
+    <RoomProvider
+      id={roomId}
+      initialPresence={{ cursor: null, selection: [] }}
+      initialStorage={{
+        layers: new LiveMap<string, LiveObject<Layer>>(),
+        layerIds: new LiveList(),
+      }}
+    >
       <ClientSideSuspense fallback={fallback}>
         {() => children}
       </ClientSideSuspense>

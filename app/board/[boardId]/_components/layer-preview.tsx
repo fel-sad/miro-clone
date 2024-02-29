5a0b43b4ee6134409074bddb3bae019eaa4a0c92
Felix Sadowski <sadowski.felix@gmail.com>
2024-02-29 17:28:11 +0100

    added text layer

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -5,6 +5,7 @@ import { LayerType } from '@/types/canvas';
 import { memo } from 'react';
 import { Rectangle } from './rectangle';
 import { Ellipse } from './ellipse';
+import { Text } from './text';
 
 interface LayerPreviewProps {
   id: string;
@@ -19,7 +20,18 @@ export const LayerPreview = memo(
     if (!layer) {
       return null;
     }
+
     switch (layer.type) {
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+
       case LayerType.Ellipse:
         return (
           <Ellipse
